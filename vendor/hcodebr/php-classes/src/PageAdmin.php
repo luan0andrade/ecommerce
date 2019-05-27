19c492a975104a223e962773154817469f281b63
Luan Andrade <luanandradeti10@hotmail.com>
2019-05-27 16:37:05 -0300

    Templates

---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace Hcode;
+
+use Rain\Tpl;
+
+   class PageAdmin extends Page{
+        public function __construct($opts = array(), $tpl_dir = "/views/admin/"){
+            /* para evitar refazer todos os métodos da classe pai, utiliza-se o parent 
+            para utilizar o método propriamente dito (da classe pai)*/
+            parent:: __construct($opts, $tpl_dir);
+        }
+    }
+
+ ?>
